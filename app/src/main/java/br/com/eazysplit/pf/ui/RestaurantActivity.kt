83ef8e58bc95c31c7efbab2bf922204dfe12ea73
gustavocorreia <gustavocorreiasantos@outlook.com>
2019-04-21 22:11:26 -0300

    Novos ajustes

---
+++ app/src/main/java/br/com/eazysplit/pf/ui/RestaurantActivity.kt
@@ -0,0 +1,13 @@
+package br.com.eazysplit.pf.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.eazysplit.pf.R
+
+class RestaurantActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant)
+    }
+}
