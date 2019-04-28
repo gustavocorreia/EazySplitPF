12905c7f8b237f832aa6829a9bbdca3ff0b4ab13
gustavocorreia <gustavocorreiasantos@outlook.com>
2019-04-28 13:23:36 -0300

    About Screen

--- app/src/main/java/br/com/eazysplit/pf/ui/MainActivity.kt
+++ app/src/main/java/br/com/eazysplit/pf/ui/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navView: BottomNavigationView = findViewById(R.id.navMenu)
 
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
