3388502e87c208f530e4b0756bb8df841034b2a7
gustavocorreia <gustavocorreiasantos@outlook.com>
2019-05-03 00:48:57 -0300

    Cadastro de clientes

--- app/src/main/java/br/com/eazysplit/pf/ui/CustomerActivity.kt
+++ app/src/main/java/br/com/eazysplit/pf/ui/CustomerActivity.kt
@@ -2,7 +2,9 @@ package br.com.eazysplit.pf.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.eazysplit.pf.R
+import br.com.eazysplit.pf.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_customer.*
@@ -12,10 +14,22 @@ class CustomerActivity : AppCompatActivity() {
     private lateinit var mAuth : FirebaseAuth
     private lateinit var mDB: FirebaseDatabase
 
+
     override fun onStart() {
         super.onStart()
 
         mAuth = FirebaseAuth.getInstance()
+        mDB = FirebaseDatabase.getInstance()
+
+        loadForm()
+    }
+
+    private fun loadForm(){
+        val currentUser = mAuth.currentUser
+        if(currentUser != null) {
+            etEmail.setText(currentUser!!.email)
+
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +41,28 @@ class CustomerActivity : AppCompatActivity() {
 
     fun registerCustomer(){
         btRegister.setOnClickListener {
+            if(validateFields()){
 
+            }
         }
     }
 
     private fun validateFields() : Boolean{
+        if (etName.text.toString() == ""
+            || etEmail.text.toString() == ""
+            || etPassword.text.toString() == ""
+            || etPhone.text.toString() == ""
+            || etBirthDate.text.toString() == ""
+            || etPasswordConfirm.text.toString() == "") {
+            Toast.makeText(this, R.string.text_fill_fields, Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        if(etPassword.text.toString() != etPasswordConfirm.text.toString()){
+            Toast.makeText(this, R.string.text_divergent_passwords, Toast.LENGTH_LONG).show()
+            return false
+        }
+
         return true
     }
 }
