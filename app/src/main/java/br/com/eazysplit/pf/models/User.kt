028dc94d95c55198970c90bd8757d533d4a617ae
gustavocorreia <gustavocorreiasantos@outlook.com>
2019-05-04 21:21:04 -0300

    Edição de dados

--- app/src/main/java/br/com/eazysplit/pf/models/User.kt
+++ app/src/main/java/br/com/eazysplit/pf/models/User.kt
@@ -1,29 +1,37 @@
 package br.com.eazysplit.pf.models
 
 import android.arch.persistence.room.*
+import com.google.firebase.database.Exclude
 import com.google.gson.annotations.Expose
 import java.util.*
 
 @Entity
 data class User (
+    @Exclude
     @PrimaryKey
     var id: String,
 
+    @Exclude
     var name: String,
 
+    @Exclude
     var email: String,
 
-    @Expose
+    @Expose // Será gravado no Firebase
     var phoneNumber: String,
 
-    @Expose
+    @Expose // Será gravado no Firebase
     var birthDate: Date,
 
+    @Exclude
     var password: String,
 
+    @Exclude
     var url_image: String?,
 
+    @Exclude
     var CardList: List<Card>?,
 
+    @Exclude
     var lastUpdate: Date?
 )
