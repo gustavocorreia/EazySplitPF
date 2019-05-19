3cea82c26e2011097bd76872a94468721f620fdd
Dynara <dynara.oliveira@gmail.com>
2019-05-19 17:46:50 -0300

    corrige conflitos

--- app/src/main/java/br/com/eazysplit/pf/SettingsFragment.kt
+++ app/src/main/java/br/com/eazysplit/pf/SettingsFragment.kt
@@ -6,27 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.content.Intent
+import android.widget.Button
 import br.com.eazysplit.pf.ui.CardListActivity
 import br.com.eazysplit.pf.ui.CustomerActivity
 import br.com.eazysplit.pf.ui.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_settings.*
 
-class SettingsFragment : Fragment() {
+class SettingsFragment: Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
-        return view
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         val mAuth = FirebaseAuth.getInstance()
 
+        val btEditCustomer = view.findViewById(R.id.btEditCustomer) as Button
+        val btLanguage = view.findViewById(R.id.btLanguage) as Button
+        val btPaymentMethod = view.findViewById(R.id.btPaymentMethod) as Button
+        val btExit = view.findViewById(R.id.btExit) as Button
 
         btEditCustomer.setOnClickListener {
             val user = mAuth.currentUser
@@ -49,6 +50,8 @@ class SettingsFragment : Fragment() {
             startActivity(loginIntent)
             activity!!.finish()
         }
+
+        return view
     }
 
 }
